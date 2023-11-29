4a91d3a1fa8361f5490048a0a34dededa6b315b7
drew <drewtuannguyen2003@gmail.com>
2023-11-29 16:06:47 -0500

    change

--- js/Signup.js
+++ js/Signup.js
@@ -1,4 +1,17 @@
 var firebaseConfig = {
+  apiKey: "AIzaSyA6xF9mCCjNkJVp0zFVhXr9j93vflXa7Yo",
+
+  authDomain: "csci-225-assignment-8-project.firebaseapp.com",
+
+  projectId: "csci-225-assignment-8-project",
+
+  storageBucket: "csci-225-assignment-8-project.appspot.com",
+
+  messagingSenderId: "557733923398",
+
+  appId: "1:557733923398:web:67b961c036306a431ed847",
+
+  measurementId: "G-QT2RSG5EMR"
 
 };
 // Initialize Firebase
@@ -20,6 +33,7 @@ $("#signup-form").submit(function(e) {
   console.log(username,email,password,confirmpassword);
 
   // create a user with email address and password
+  
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
@@ -30,9 +44,28 @@ $("#signup-form").submit(function(e) {
       user.updateProfile({
         displayName: username,
         displayEmail: email
-      });
-      console.log(user.displayName,"You are signed up");
+      }).then(()=>{
+        console.log(user.displayName,"You are signed up");
       //window.location.href = "Login.html";
+
+      var date = "Wed, 21 NOV 2023 12:57";
+
+      var userinformation = {
+        // list what you want to put in
+        "username": user.displayName,
+        "email": email,
+        "signupDate": date
+      };
+
+      var db = firebase.firestore();
+      db.collection("Player Table").doc(user.displayName).set(userinformation).then(() => {
+        console.log("information saved to firestore");
+        window.location.href = "Login.html";
+      }); 
+
+
+      });
+       // put the information after you callback (then())
       
     })
     .catch(error => {
